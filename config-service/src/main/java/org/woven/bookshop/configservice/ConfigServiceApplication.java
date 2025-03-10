ce0208771a6eb257285a4ff62bf3ad9b4ce94725
Eswara <eswarap@outlook.in>
2025-03-10 17:22:21 +0530

    config repo

---
+++ config-service/src/main/java/org/woven/bookshop/configservice/ConfigServiceApplication.java
@@ -0,0 +1,12 @@
+package org.woven.bookshop.configservice;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServiceApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
