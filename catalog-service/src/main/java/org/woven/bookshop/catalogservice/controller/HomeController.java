ef4e52108557ba3c1e6903a945a53e636585909d
Eswara <eswarap@outlook.in>
2025-03-10 13:37:12 +0530

    new property

--- catalog-service/src/main/java/org/woven/bookshop/catalogservice/controller/HomeController.java
+++ catalog-service/src/main/java/org/woven/bookshop/catalogservice/controller/HomeController.java
@@ -2,11 +2,18 @@ package org.woven.bookshop.catalogservice.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.woven.bookshop.catalogservice.config.AppProperties;
 
 @RestController
 public class HomeController {
+    private final AppProperties appProperties;
+
+    public HomeController(AppProperties appProperties) {
+        this.appProperties = appProperties;
+    }
+
     @GetMapping("/")
     public String getGreeting() {
-        return "Welcome to book shop catalog";
+        return appProperties.getGreeting();
     }
 }
