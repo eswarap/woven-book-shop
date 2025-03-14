1b26b62b36c3dde07dca2b9adcf60de5ce80fd60
eswara <eswarap@outlook.in>
2025-03-14 10:04:14 +0530

    integration test and profile

--- catalog-service/src/test/java/org/woven/bookshop/catalogservice/CatalogServiceApplicationTests.java
+++ catalog-service/src/test/java/org/woven/bookshop/catalogservice/CatalogServiceApplicationTests.java
@@ -3,11 +3,13 @@ package org.woven.bookshop.catalogservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.woven.bookshop.catalogservice.domain.Book;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@ActiveProfiles("integration")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CatalogServiceApplicationTests {
 
